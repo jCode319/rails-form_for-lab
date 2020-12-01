15945731bc1eb4d6368014dc2a5908e3c934e6d3
jCode319 <jdjack3@gmail.com>
2020-11-30 20:41:32 -0500

    commit tree 8e8867892afcdc62649dfbcd3cf645d49072a455

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,29 +4,31 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def edit
-
-  end
-
   def new
     @student = Student.new
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
   def create
-    @student = Student.new(student_params(:first_name, :last_name))
+    @student = Student.new(student_params)
     @student.save
     redirect_to student_path(@student)
   end
 
   def update
-
+    @student = Student.find(params[:id])
+    @student = Student.update(student_params)
+    redirect_to student_path(@student)
   end
 
 
   private
 
-    def student_params(*args)
-      params.require(:student).permit(*args)
+    def student_params
+      params.require(:student).permit!
     end
 
 end
